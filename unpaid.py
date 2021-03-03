0373bc11e4509f2d119e3f66f9a2bb351fb5bb12
Estacaco <44181620+Estacaco@users.noreply.github.com>
2021-03-02 19:26:30 -0600

    Add files via upload

---
+++ unpaid.py
@@ -0,0 +1,21 @@
+import nicehash
+import json
+import requests
+
+def get_json(endpoint):
+    resp = requests.get(endpoint)
+    assert resp.status_code == 200
+    result = json.loads(resp.content.decode('utf8').replace("'", '"'))
+    assert not 'error' in result['result']
+    return result
+
+host = 'https://api2.nicehash.com'
+organisation_id = 'Organization'
+key = 'Public Key'
+secret = 'Secret Key' 
+
+private_api = nicehash.private_api(host, organisation_id, key, secret)
+
+unpaid = private_api.get_unpaid()
+
+print(unpaid['data'])
